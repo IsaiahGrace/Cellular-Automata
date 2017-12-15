f6399396a947789b87fde7687642b0143e42c9b5
Isaiah Grace <igrace@purdue.edu>
2017-12-15 00:59:46 -0500

    Creates a maze and fills it in with blue.

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #define CYAN    "\x1b[36m"
 #define BLACK   "\x1b[22"
 #define RESET   "\x1b[0m"
+#define PURDUE  "\x1b[38;2;194;142;14m"
 #endif
 
 #ifndef COLOR
@@ -28,7 +29,7 @@
 #define BLACK   ""
 #define RESET   ""
 #endif
-//#define PURDUE  "\x1b[38;2;<194>;<142>;<14>m"
+
 
 int main(int argc, char **argv) {
 
@@ -36,6 +37,8 @@ int main(int argc, char **argv) {
   int j;
   int k;
   int crowd;
+  int diagonal;
+  int orthoganal;
   int alive;
   int height; //ascociate with i
   int width; //ascociate with j
@@ -46,7 +49,7 @@ int main(int argc, char **argv) {
     width = atoi(argv[2]);
   } else {
     struct winsize size;
-  	ioctl( 0, TIOCGWINSZ, (char *) &size );
+    ioctl( 0, TIOCGWINSZ, (char *) &size );
     //printf( "Rows: %u\nCols: %u\n", size.ws_row, size.ws_col);
     height = size.ws_row - 1;
     width = size.ws_col + 2;
@@ -72,10 +75,10 @@ int main(int argc, char **argv) {
   for(i = 0; i < height; i++) {
     for(j = 0; j < width; j++) {
       if(i == 0 || j == 0 || i == (height - 1) || j == (width - 1)) {
-      map[i][j] = 0;
-    } else {
-	map[i][j] = 0; //(rand() & 1);
-    }
+        map[i][j] = 0;
+      } else {
+        map[i][j] = 0; //(rand() & 1);
+      }
       next[i][j] = 0;
       prev[i][j] = 0;
     }
@@ -83,67 +86,72 @@ int main(int argc, char **argv) {
   }
   printf("\033[1A");
 
+  //make a sqare in the middle
+  //map[height / 2][width / 2] = 1;
+
   //Hardcode a glider
-  /*
-  map[1][2] = 1;
-  map[2][3] = 1;
-  map[3][1] = 1;
-  map[3][2] = 1;
-  map[3][3] = 1;
-  */
+  {/*
+   map[1][2] = 1;
+   map[2][3] = 1;
+   map[3][1] = 1;
+   map[3][2] = 1;
+   map[3][3] = 1;
+   */}
 
   //Hardcode a glider gun
-  /*
-  map[6][2] = 1;
-  map[7][2] = 1;
-  map[6][3] = 1;
-  map[7][3] = 1;
+  {
 
-  map[6][12] = 1;
-  map[7][12] = 1;
-  map[8][12] = 1;
-  map[5][13] = 1;
-  map[9][13] = 1;
-  map[4][14] = 1;
-  map[10][14] = 1;
-  map[4][15] = 1;
-  map[10][15] = 1;
+    map[6][2] = 1;
+    map[7][2] = 1;
+    map[6][3] = 1;
+    map[7][3] = 1;
 
-  map[7][16] = 1;
+    map[6][12] = 1;
+    map[7][12] = 1;
+    map[8][12] = 1;
+    map[5][13] = 1;
+    map[9][13] = 1;
+    map[4][14] = 1;
+    map[10][14] = 1;
+    map[4][15] = 1;
+    map[10][15] = 1;
 
-  map[5][17] = 1;
-  map[9][17] = 1;
-  map[6][18] = 1;
-  map[7][18] = 1;
-  map[8][18] = 1;
-  map[7][19] = 1;
+    map[7][16] = 1;
 
-  map[4][22] = 1;
-  map[5][22] = 1;
-  map[6][22] = 1;
-  map[4][23] = 1;
-  map[5][23] = 1;
-  map[6][23] = 1;
-  map[3][24] = 1;
-  map[7][24] = 1;
-  map[2][26] = 1;
-  map[3][26] = 1;
-  map[7][26] = 1;
-  map[8][26] = 1;
+    map[5][17] = 1;
+    map[9][17] = 1;
+    map[6][18] = 1;
+    map[7][18] = 1;
+    map[8][18] = 1;
+    map[7][19] = 1;
 
-  map[4][36] = 1;
-  map[5][36] = 1;
-  map[4][37] = 1;
-  map[5][37] = 1;
-  */
+    map[4][22] = 1;
+    map[5][22] = 1;
+    map[6][22] = 1;
+    map[4][23] = 1;
+    map[5][23] = 1;
+    map[6][23] = 1;
+    map[3][24] = 1;
+    map[7][24] = 1;
+    map[2][26] = 1;
+    map[3][26] = 1;
+    map[7][26] = 1;
+    map[8][26] = 1;
+
+    map[4][36] = 1;
+    map[5][36] = 1;
+    map[4][37] = 1;
+    map[5][37] = 1;
+
+  }
 
   //Start LIFE
   for(k = 0; k < 1000; k++) {
     //Print Map as is
     alive = 0;
     for(i = 0; i < height - 1; i++) {
-    printf("\033[1A");
-  }
+      printf("\033[1A");
+    }
     printf("%d\n", k);
     for(i = 1; i < height - 1; i++) {
       for(j = 1; j < width - 1; j++) {
@@ -158,7 +166,7 @@ int main(int argc, char **argv) {
           if(prev[i][j]) {
             printf(""RED"\u2591");
           } else {
-          printf(""RESET"\u2591");
+            printf(""RESET"\u2591");
           }
         }
       }
@@ -171,82 +179,80 @@ int main(int argc, char **argv) {
       for(j = 1; j < width - 1; j++) {
         //Count Neighbours
         crowd = 0;
-        if(map[i - 1][j - 1]) {crowd++;}
-        if(map[i][j - 1]) {crowd++;}
-        if(map[i + 1][j - 1]) {crowd++;}
-
-        if(map[i - 1][j + 1]) {crowd++;}
-        if(map[i][j + 1]) {crowd++;}
-        if(map[i + 1][j + 1]) {crowd++;}
-
-        if(map[i + 1][j]) {crowd++;}
-        if(map[i - 1][j]) {crowd++;}
-
+        diagonal = 0;
+        orthoganal = 0;
+        if(map[i - 1][j - 1]) {crowd++; diagonal++;}
+        if(map[i + 1][j - 1]) {crowd++; diagonal++;}
+        if(map[i - 1][j + 1]) {crowd++; diagonal++;}
+        if(map[i + 1][j + 1]) {crowd++; diagonal++;}
+        if(map[i][j + 1]) {crowd++; orthoganal++;}
+        if(map[i][j - 1]) {crowd++; orthoganal++;}
+        if(map[i + 1][j]) {crowd++; orthoganal++;}
+        if(map[i - 1][j]) {crowd++; orthoganal++;}
         //Update next with values
-	if(map[i][j]) {
-	  switch(crowd) {
-	  case 0:
-	    next[i][j] = 0;
-	    break;
-	  case 1:
-	    next[i][j] = 1;
-	    break;
-	  case 2:
-	    next[i][j] = 1;
-	    break;
-	  case 3:
-	    next[i][j] = 1;
-	    break;
-	  case 4:
-	    next[i][j] = 1;
-	    break;
-	  case 5:
-	    next[i][j] = 1;
-	    break;
-	  case 6:
-	    next[i][j] = 0;
-	    break;
-	  case 7:
-	    next[i][j] = 0;
-	    break;
-	  case 8:
-	    next[i][j] = 0;
-	    break;
-	  }
-	} else {
-	  switch(crowd) {
-	  case 0:
-	    next[i][j] = 0;
-	    break;
-	  case 1:
-	    next[i][j] = 0;
-	    break;
-	  case 2:
-	    next[i][j] = 0;
-	    break;
-	  case 3:
-	    next[i][j] = 1;
-	    break;
-	  case 4:
-	    next[i][j] = 0;
-	    break;
-	  case 5:
-	    next[i][j] = 0;
-	    break;
-	  case 6:
-	    next[i][j] = 0;
-	    break;
-	  case 7:
-	    next[i][j] = 0;
-	    break;
-	  case 8:
-	    next[i][j] = 0;
-	    break;
-	  }
-	}
+        if(map[i][j]) {
+          switch(crowd) {
+            case 0:
+              next[i][j] = 0;
+              break;
+            case 1:
+              next[i][j] = 1;
+              break;
+            case 2:
+              next[i][j] = 1;
+              break;
+            case 3:
+              next[i][j] = 1;
+              break;
+            case 4:
+              next[i][j] = 1;
+              break;
+            case 5:
+              next[i][j] = 1;
+              break;
+            case 6:
+              next[i][j] = 0;
+              break;
+            case 7:
+              next[i][j] = 0;
+            break;
+              case 8:
+              next[i][j] = 0;
+            break;
+          }
+        } else {
+          switch(crowd) {
+            case 0:
+              next[i][j] = 0;
+              break;
+            case 1:
+              next[i][j] = 0;
+              break;
+            case 2:
+              next[i][j] = 0;
+              break;
+            case 3:
+              next[i][j] = 1;
+              break;
+            case 4:
+              next[i][j] = 0;
+              break;
+            case 5:
+              next[i][j] = 0;
+              break;
+            case 6:
+              next[i][j] = 0;
+              break;
+            case 7:
+              next[i][j] = 0;
+              break;
+            case 8:
+              next[i][j] = 0;
+              break;
+          }
+        }
       }
     }
-
     //Transfer next values to map and reset next
     alive = 0;
     for(i = 0; i < height - 1; i++) {
@@ -259,8 +265,127 @@ int main(int argc, char **argv) {
     }
     if(!alive) {break;}
     //wait one second 1,000,000 = 1 sec
+    //usleep(10000);
+  }
+
+  //Just make sure everything is either a 1 or 0
+  for(i = 1; i < height - 1; i++) {
+    for(j = 1; j < width - 1; j++) {
+      if(map[i][j]) {map[i][j] = 1;}
+      prev[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+  //starting position to explore maze
+  map[height / 2][width / 2] = 2;
+
+  //Fill in Maze
+  for(k = 0; k < 1000; k++) {
+    //Print Map as is
+    alive = 0;
+    for(i = 0; i < height - 1; i++) {
+      printf("\033[1A");
+    }
+    printf("%d\n", k);
+    for(i = 1; i < height - 1; i++) {
+      for(j = 1; j < width - 1; j++) {
+        if(map[i][j] == 1) {
+          printf(""RESET"\u2588");
+        }
+        if(map[i][j] == 2) {
+          if(prev[i][j] == 2) {
+            printf(""BLUE"\u2588");
+          } else {
+            printf(""CYAN"\u2588");
+            alive = 1;
+          }
+        }
+        if(map[i][j] == 0) {
+          printf(""RESET"\u2591");
+        }
+      }
+      printf("\n");
+    }
+    if(!alive) {break;}
+
+    //Update The next map
+    for(i = 1; i < height - 1; i++) {
+      for(j = 1; j < width - 1; j++) {
+        //Count Neighbours
+        crowd = 0;
+        if(map[i - 1][j - 1] == 2) {crowd++;}
+        if(map[i + 1][j - 1] == 2) {crowd++;}
+        if(map[i - 1][j + 1] == 2) {crowd++;}
+        if(map[i + 1][j + 1] == 2) {crowd++;}
+        if(map[i][j + 1] == 2) {crowd++;}
+        if(map[i][j - 1] == 2) {crowd++;}
+        if(map[i + 1][j] == 2) {crowd++;}
+        if(map[i - 1][j] == 2) {crowd++;}
+
+        //Update next with values
+        if(map[i][j] == 0){
+          switch(crowd) {
+            case 0:
+              next[i][j] = 0;
+              break;
+            case 1:
+              next[i][j] = 2;
+              break;
+            case 2:
+              next[i][j] = 2;
+              break;
+            case 3:
+              next[i][j] = 2;
+              break;
+            case 4:
+              next[i][j] = 2;
+              break;
+            case 5:
+              next[i][j] = 2;
+              break;
+            case 6:
+              next[i][j] = 2;
+              break;
+            case 7:
+              next[i][j] = 2;
+              break;
+            case 8:
+              next[i][j] = 2;
+              break;
+          }
+        } else {
+          next[i][j] = map[i][j];
+        }
+      }
+    }
+
+    //Transfer next values to map and reset next
+    for(i = 0; i < height - 1; i++) {
+      for(j = 0; j < width - 1; j++) {
+        prev[i][j] = map[i][j];
+        map[i][j] = next[i][j];
+        next[i][j] = 0;
+      }
+    }
+    //wait one second 1,000,000 = 1 sec
     usleep(10000);
   }
+
+    usleep(5000000);
+    for(i = 0; i < height - 1; i++) {
+      printf("\033[1A");
+    }
+    printf("%d\n", k);
+    for(i = 1; i < height - 1; i++) {
+      for(j = 1; j < width - 1; j++) {
+        if(map[i][j] == 2) {
+          printf(""BLUE"\u2588");
+        } else {
+          printf(""RESET"\u2591");
+        }
+      }
+      printf("\n");
+    }
 
   //Free malloc'd memory
   for(i = 0; i < height; i++) {
